1c7ab62662a1fd1059980c13fee60ab757224197
Lluis Casamajor <lluischess@gmail.com>
2021-05-28 15:37:34 +0200

    add nav and style

--- gymluis/functions.php
+++ gymluis/functions.php
@@ -1,5 +1,15 @@
 <?php
 # Importante que todas las funciones del tema añadan el nombre del tema al principio
+
+# Scripts and Styles
+function gymluis_scripts_styles(){
+    wp_enqueue_style('style.css', get_stylesheet_uri() , array(), '1.0.0');
+    wp_enqueue_style('normalize.css', get_template_directory_uri().'/css/normalize.css', array(), '8.0.1');
+}
+
+add_action( 'wp_enqueue_scripts', 'gymluis_scripts_styles');
+
+# Navegation Nav 
 function gymluis_nav() {
     register_nav_menus( array(
         'main-nav' => __( 'Main menu', 'Gymluis'),
@@ -8,6 +18,10 @@ function gymluis_nav() {
 }
 
 add_action( 'init', 'gymluis_nav');
+
+
+
+
 
 
 ?>
--- gymluis/header.php
+++ gymluis/header.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php wp_head() ?>
 </head>
 <body>
 <header>
@@ -13,16 +14,16 @@
             <!-- search template ubication folder to img logo -->
                 <img src="<?php echo get_template_directory_uri() ?>\img\logo.svg" alt="logo">
             </div>
-            <nav id="screen_nav">
-                <ul>
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">Entrenadores</a></li>
-                    <li><a href="#">Galeria</a></li>
-                    <li><a href="#">Blog</a></li>
-                    <li><a href="#">Sobre Nosotros</a></li>
-                    <li><a href="#">Contacto</a></li>
-                </ul>
-            </nav>
+            <?php 
+
+            $gymluis_nav = array(
+                'theme_location' => 'main-nav',
+                'container' => 'nav',
+                'container_class' => 'main-nav'
+            );
+            wp_nav_menu($gymluis_nav);
+            
+            ?>
         </div>
     </div>
 </header>
