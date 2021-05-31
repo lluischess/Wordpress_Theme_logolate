8551ce3883836138812cbc5bf478ee74a6169f22
Lluis Casamajor <lluischess@gmail.com>
2021-05-31 20:05:23 +0200

    Adaptar tema para Logolate

--- logolate-byluis/functions.php
+++ logolate-byluis/functions.php
@@ -2,22 +2,21 @@
 # Importante que todas las funciones del tema añadan el nombre del tema al principio
 
 # Scripts and Styles
-function gymluis_scripts_styles(){
+function byluis_scripts_styles(){
     wp_enqueue_style('style.css', get_stylesheet_uri() , array(), '1.0.0');
     wp_enqueue_style('normalize.css', get_template_directory_uri().'/css/normalize.css', array(), '8.0.1');
 }
 
-add_action( 'wp_enqueue_scripts', 'gymluis_scripts_styles');
+add_action( 'wp_enqueue_scripts', 'byluis_scripts_styles');
 
 # Navegation Nav 
-function gymluis_nav() {
+function byluis_nav() {
     register_nav_menus( array(
-        'main-nav' => __( 'Main menu', 'Gymluis'),
-        'second-nav' => __( 'Secondary menu', 'Gymluis')
+        'main-nav' => __( 'Main menu', 'Logolate')
     ) );
 }
 
-add_action( 'init', 'gymluis_nav');
+add_action( 'init', 'byluis_nav');
 
 
 
