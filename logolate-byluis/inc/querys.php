a51ea8305e4de3ca56124054a839ae583c0b3209
Lluis Casamajor <lluischess@gmail.com>
2021-06-21 23:32:47 +0200

    while catalogos

--- logolate-byluis/inc/querys.php
+++ logolate-byluis/inc/querys.php
@@ -1,7 +1,33 @@
 <?php
 
-function byluis_lista_catalogos(){
-    echo "hola";
-}
+function byluis_lista_catalogos(){ ?>
 
+<ul class="lista-catalogos">
+    <?php
+        $args = array(
+            'post_type' => 'Luis_logolate_clases',
+            'post_pe_page' => 100
+        );
+        $catalogos = new WP_Query($args);
+        while( $catalogos->have_posts()){
+            $catalogos->the_post(); ?>
+        <li class="catalogo card">
+            <?php if (has_post_thumbnail()){
+                    the_post_thumbnail('medium', array( 'class' => 'default-img'));
+                } ?>
+            <div class="container-card">
+                <a href="<?php the_permalink(); ?>">
+                    <h2 class=""><?php the_title(); ?></h2>
+                </a>
+                <p class="p-catalogo-card"><?php the_field('resumen'); ?></p>
+            </div>
+            
+            
+        </li>
+    <?php } wp_reset_postdata(); ?>
+</ul>
+
+
+<?php
+}
 ?>
