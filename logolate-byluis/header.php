2ca61dafe7213fa931555b18ec90041b539bc282
Lluis Casamajor <lluischess@gmail.com>
2021-06-30 23:07:54 +0200

    finish slider

--- logolate-byluis/header.php
+++ logolate-byluis/header.php
@@ -26,11 +26,12 @@
             ?>
         </div>
     </div>
+    <!-- Slider, it can by possible change imgs or add more sliders 4 -->
         <div id="L-Slider">
-            <img class="fondo mySlides" src="<?php echo get_template_directory_uri() ?>\img\Caramelos-personalizados.png">
-            <img class="fondo mySlides" src="<?php echo get_template_directory_uri() ?>\img\Chocolates-personalizados.png">
-            <img class="fondo mySlides" src="<?php echo get_template_directory_uri() ?>\img\Caramelos-personalizados.png">
-            <img class="fondo mySlides" src="<?php echo get_template_directory_uri() ?>\img\Chocolates-personalizados.png">
+            <img class="fondo mySlides bloc1" src="<?php echo get_template_directory_uri() ?>\img\Caramelos-personalizados.png">
+            <img class="fondo mySlides bloc" src="<?php echo get_template_directory_uri() ?>\img\Chocolates-personalizados.png">
+            <img class="fondo mySlides bloc" src="<?php echo get_template_directory_uri() ?>\img\Caramelos-personalizados.png">
+            <img class="fondo mySlides bloc" src="<?php echo get_template_directory_uri() ?>\img\Chocolates-personalizados.png">
 
             <button class="button Lblack display-left" onclick="plusDivs(-1)">&#10094;</button>
             <button class="button Lblack display-right" onclick="plusDivs(1)">&#10095;</button>
